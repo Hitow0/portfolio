c8b5e97bc76d9cab6d3180f3083dc43106ad42b2
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-07 11:34:18 +0100

    :sparkles: Ajout de la page principale

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+import styles from '../app/css/page.module.css'
+
+export default function MainPage() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.accueil}>
+                <h1>Nicolas BLART</h1>
+                <p>Etudiant en Informatique</p>
+            </div>
+        </main>
+    )
+}
