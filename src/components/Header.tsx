0c27fa4e8155a1e36bea49b8d198ac42f71d0e3a
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-07 12:52:14 +0100

    :sparkles: Ajout des ancres et redirection à celle-ci

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@
 import React, {useEffect, useState} from 'react';
 import styles from '../app/css/navbar.module.css'
 import Modal from './ModalComponent';
-import Image from "next/image";
 
 interface NavbarProps {
     isExpanded: boolean;
@@ -45,6 +44,110 @@ const Header = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const scrollToAccueil = () => {
+            const accueilElement = document.getElementById('accueil');
+
+            if (accueilElement) {
+                accueilElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        };
+
+        const linkElement = document.getElementById('accueil-link');
+        if (linkElement) {
+            linkElement.addEventListener('click', scrollToAccueil);
+        }
+
+        return () => {
+            if (linkElement) {
+                linkElement.removeEventListener('click', scrollToAccueil);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollToApropos = () => {
+            const accueilElement = document.getElementById('apropos');
+
+            if (accueilElement) {
+                accueilElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        };
+
+        const linkElement = document.getElementById('apropos-link');
+        if (linkElement) {
+            linkElement.addEventListener('click', scrollToApropos);
+        }
+
+        return () => {
+            if (linkElement) {
+                linkElement.removeEventListener('click', scrollToApropos);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollToApropos = () => {
+            const accueilElement = document.getElementById('competences');
+
+            if (accueilElement) {
+                accueilElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        };
+
+        const linkElement = document.getElementById('competences-link');
+        if (linkElement) {
+            linkElement.addEventListener('click', scrollToApropos);
+        }
+
+        return () => {
+            if (linkElement) {
+                linkElement.removeEventListener('click', scrollToApropos);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollToApropos = () => {
+            const accueilElement = document.getElementById('parcours');
+
+            if (accueilElement) {
+                accueilElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        };
+
+        const linkElement = document.getElementById('parcours-link');
+        if (linkElement) {
+            linkElement.addEventListener('click', scrollToApropos);
+        }
+
+        return () => {
+            if (linkElement) {
+                linkElement.removeEventListener('click', scrollToApropos);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        const scrollToApropos = () => {
+            const accueilElement = document.getElementById('projets');
+
+            if (accueilElement) {
+                accueilElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        };
+
+        const linkElement = document.getElementById('projets-link');
+        if (linkElement) {
+            linkElement.addEventListener('click', scrollToApropos);
+        }
+
+        return () => {
+            if (linkElement) {
+                linkElement.removeEventListener('click', scrollToApropos);
+            }
+        };
+    }, []);
 
     return (
         <div className={`${isNavReduced ? styles.navbarReduced : styles.navbar}`}>
@@ -63,23 +166,25 @@ const Header = () => {
                 <button onClick={handleDownloadCV}>Télécharger le CV</button>
             </Modal>
 
-            <div className={styles.navbutton}>
+            <div id={"accueil-link"} className={styles.navbutton}>
                 <img src={"/assets/navbar/accueil.svg"} alt={"Accueil"}/>
                 <p>Accueil</p>
             </div>
-            <div className={styles.navbutton}>
+
+            <div id={"apropos-link"} className={styles.navbutton}>
                 <img src={"/assets/navbar/apropos.svg"} alt={"A propos"}/>
                 <p>A propos</p>
             </div>
-            <div className={styles.navbutton}>
+
+            <div id={"competences-link"} className={styles.navbutton}>
                 <img src={"/assets/navbar/competences.svg"} alt={"Compétences"}/>
                 <p>Compétences</p>
             </div>
-            <div className={styles.navbutton}>
+            <div id={"parcours-link"} className={styles.navbutton}>
                 <img src={"/assets/navbar/parcours.svg"} alt={"Parcours"}/>
                 <p>Parcours</p>
             </div>
-            <div className={styles.navbutton}>
+            <div id={"projets-link"} className={styles.navbutton}>
                 <img src={"/assets/navbar/project.svg"} alt={"Projets"}/>
                 <p>Projets</p>
             </div>
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,7 +2,7 @@ import styles from '../app/css/page.module.css'
 
 export default function MainPage() {
     return (
-        <main className={styles.main}>
+        <main className={styles.main} id={'accueil'}>
             <div className={styles.accueil}>
                 <h1>Nicolas BLART</h1>
                 <p>Etudiant en Informatique</p>
