9d3f08c8e28f59e03515f0816d4e495c5702f181
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-08 04:47:14 +0100

    :bug: bug dans l'export

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-// next.config.js
-module.exports = {
-    // Autres configurations...
-    generateStaticParams: async () => {
-        // Retournez les paramètres statiques souhaités sous forme d'objet.
-        return {
-            '/': { page: '/' },
-            // Ajoutez d'autres routes si nécessaire
-            // ...
-        };
-    },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+
+module.exports = nextConfig
