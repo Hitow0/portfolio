9ea682876cb893f7d934956635b119b31d705363
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-08 04:41:57 +0100

    :bug: Bug dans l'export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    // Autres configurations...
+    generateStaticParams: async () => {
+        // Retournez les paramètres statiques souhaités sous forme d'objet.
+        return {
+            '/': { page: '/' },
+            // Ajoutez d'autres routes si nécessaire
+            // ...
+        };
+    },
+};
